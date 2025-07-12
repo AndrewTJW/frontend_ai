a42ca72f40ec4c0b62145d7877180a98802aa3ca
AndrewTJW <andrewtjw26@gmail.com>
2025-07-13 02:22:40 +0800

    Added new UI features

--- src/components/InputStock.jsx
+++ src/components/InputStock.jsx
@@ -70,7 +70,7 @@ const InputStock = () => {
           </div>
           <button
             type="submit"
-            className="text-black border border-black hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-6 py-2.5 text-center"
+            className="text-black border border-black hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-6 py-2.5 text-center font-formula-regular"
           >
             {loading ? 'Predicting...' : 'Submit'}
           </button>
